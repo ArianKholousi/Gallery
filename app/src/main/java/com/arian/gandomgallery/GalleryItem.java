56d520a36fee5dd666b6bf4f047ce511aec8e12d
Arian <ariankholousi@gmail.com>
2018-04-18 14:52:36 +0430

    create recyclerView with gridLayout. add and use volley ,picasso and Gson libraries.

--- app/src/main/java/com/arian/gandomgallery/GalleryItem.java
+++ app/src/main/java/com/arian/gandomgallery/GalleryItem.java
@@ -1,8 +1,29 @@
 package com.arian.gandomgallery;
 
+import java.util.List;
+
 /**
  * Created by Payami on 04/17/2018.
  */
 
 public class GalleryItem {
+
+    private String description;
+    private List<String> images;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
 }
--- app/src/main/java/com/arian/gandomgallery/MainFragment.java
+++ app/src/main/java/com/arian/gandomgallery/MainFragment.java
@@ -5,11 +5,25 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,6 +34,9 @@ public class MainFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private PhotoAdapter photoAdapter;
+    private List<GalleryItem> galleryItems;
+    private JsonObjectRequest jsonObjectRequest;
+    private String url;
 
     public MainFragment() {
         // Required empty public constructor
@@ -33,6 +50,8 @@ public class MainFragment extends Fragment {
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,6 +59,32 @@ public class MainFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_main, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_gallery_items);
+
+        galleryItems = new ArrayList<>();
+        url = "http://gandom.co/devTest/1/home";
+
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("list");
+                    galleryItems = new Gson().fromJson(jsonArray.toString(), new TypeToken<List<GalleryItem>>(){}.getType());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        SingletonRequestQueue.getInstance(getActivity()).getRequestQueue().add(jsonObjectRequest);
+
+
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         updateRecyclerView();
 
@@ -63,6 +108,11 @@ public class MainFragment extends Fragment {
 
         public void bindPhoto(GalleryItem item){
             this.galleryItem = item;
+
+            for (int i=0; i<item.getImages().size();i++){
+                String url = "http://gandom.co/devTest/1/images/" + item.getImages().get(i);
+                Picasso.with(getActivity()).load(url).into(imageView);
+            }
         }
     }
 
@@ -99,8 +149,10 @@ public class MainFragment extends Fragment {
     public void updateRecyclerView(){
 
         if (photoAdapter == null){
+            photoAdapter = new PhotoAdapter(galleryItems);
             recyclerView.setAdapter(photoAdapter);
         }else {
+            photoAdapter.setItemList(galleryItems);
             photoAdapter.notifyDataSetChanged();
         }
 
